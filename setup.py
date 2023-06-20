b42861da343b6c1741ec885df83e720345d6f094
ilfrich <das-peter@gmx.de>
2023-06-20 12:11:18 +1000

    Fixed setup.py to include readme content

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="oes",
-      version="1.0.1",
-      description="Optimal operation of energy storage",
+      version="1.0.2",
+      description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/solstice-ai/optimal-energy-storage",
       author="Julian de Hoog",
       author_email='julian@dehoog.ca',
